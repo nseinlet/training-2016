0321908d39933a93ff03f66e75211b18a9c81301
Nicolas Seinlet <nse@openerp.com>
2016-11-14 16:56:19 +0100

    M2M

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -6,5 +6,6 @@ class Course(models.Model):
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
+    responsible_id = fields.Many2one('res.users', string='Responsible')
     session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
     
--- openacademy/models/session.py
+++ openacademy/models/session.py
@@ -9,4 +9,6 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    attendee_ids = fields.Many2many('res.partner', 'attendees_m2m_link', 'session_id', 'partner_id')
     
