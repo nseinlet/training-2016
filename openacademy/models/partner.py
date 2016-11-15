6087028819d4e4c2c11eb1a0aef4202b9cf49b04
Nicolas Seinlet <nse@openerp.com>
2016-11-15 10:39:13 +0100

    Alter res.partner

---
+++ openacademy/models/partner.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+class Partner(models.Model):
+    _name = 'res.partner'
+    _inherit = 'res.partner'
+    
+    instructor = fields.Boolean(string="Is instructor")
+    session_ids = fields.Many2many('openacademy.session', 'attendees_m2m_link', 'partner_id', 'session_id', string="Attending sessions")
+    instructing_session_ids = fields.One2many('openacademy.session', 'instructor_id', string="Instructing sessions")
+    
