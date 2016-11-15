f89bbbc2fbe8f9529cc09d474cbea1f43b02ffa6
Nicolas Seinlet <nse@openerp.com>
2016-11-15 15:04:18 +0100

    Compute name

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -9,3 +9,11 @@ class Course(models.Model):
     responsible_id = fields.Many2one('res.users', string='Responsible')
     session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
     entity_id = fields.Many2one('openacademy.entity', string='Entity')
+    average_fill = fields.Float(string='Average fill', compute='_compute_avg_session_fill')
+    average_attendees = fields.Float(string='Average attendee count', compute='_compute_avg_session_fill')
+    
+    @api.depends('session_ids', 'session_ids.taken_seats', 'session_ids.attendee_ids')
+    def _compute_avg_session_fill(self):
+        for rec in self:
+            rec.average_fill = sum([sess.taken_seats for sess in rec.session_ids]) / len(rec.session_ids) if rec.session_ids else 0
+            rec.average_attendees = sum([len(sess.attendee_ids) for sess in rec.session_ids]) / len(rec.session_ids) if rec.session_ids else 0
--- openacademy/models/entity.py
+++ openacademy/models/entity.py
@@ -3,8 +3,10 @@ from odoo import models, fields, api
 
 class Entity(models.Model):
     _name = 'openacademy.entity'
+    _rec_name = 'full_name'
     
     name = fields.Char(required=True)
+    full_name = fields.Char(compute='_compute_full_name')
     parent_id = fields.Many2one('openacademy.entity', ondelete='restrict')
     parent_left = fields.Integer(index=True)
     parent_right = fields.Integer(index=True)
@@ -13,3 +15,11 @@ class Entity(models.Model):
     _parent_store = True
     _parent_order = 'name'
     _order = 'parent_left'
+
+    @api.depends('name', 'parent_id', 'parent_id.name')
+    def _compute_full_name(self):
+        for r in self:
+            if r.parent_id.full_name:
+                r.full_name = "%s | %s" % (r.parent_id.full_name, r.name)
+            else:
+                r.full_name = r.name
