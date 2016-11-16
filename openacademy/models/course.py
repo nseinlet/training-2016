dee28a161ffb42c76fc9a51c307e7db981f9abdb
Nicolas Seinlet <nse@openerp.com>
2016-11-16 11:23:13 +0100

    Constrains

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -12,8 +12,25 @@ class Course(models.Model):
     average_fill = fields.Float(string='Average fill', compute='_compute_avg_session_fill')
     average_attendees = fields.Float(string='Average attendee count', compute='_compute_avg_session_fill')
     
+    _sql_constraints = [
+        ('name_description_check',
+         'CHECK(name != description)',
+         "The title of the course should not be the description"),
+
+        ('name_unique',
+         'UNIQUE(name)',
+         "The course title must be unique"),
+    ]
+    
     @api.depends('session_ids', 'session_ids.taken_seats', 'session_ids.attendee_ids')
     def _compute_avg_session_fill(self):
         for rec in self:
             rec.average_fill = sum([sess.taken_seats for sess in rec.session_ids]) / len(rec.session_ids) if rec.session_ids else 0
             rec.average_attendees = sum([len(sess.attendee_ids) for sess in rec.session_ids]) / len(rec.session_ids) if rec.session_ids else 0
+
+    @api.multi
+    def copy(self, default=None):
+        new_defaults = default or {}
+        new_defaults = {'name': "Copy of %s" % self.name or ''}
+        
+        return super(Course, self).copy(new_defaults)
--- openacademy/models/session.py
+++ openacademy/models/session.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class Session(models.Model):
     _name = 'openacademy.session'
@@ -16,7 +17,7 @@ class Session(models.Model):
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
     entity_id = fields.Many2one('openacademy.entity', string='Entity')
     course_entity_id = fields.Many2one('openacademy.entity', string='Course entity', related="course_id.entity_id")
-
+    
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
         for r in self:
@@ -28,19 +29,26 @@ class Session(models.Model):
     # onchange handler
     @api.onchange('seats', 'attendee_ids')
     def _onchange_seats(self):
+        def raise_warning(title, msg):
+            return {
+                'warning': {
+                    'title': title or "",
+                    'message': msg or "",
+                }
+            }
         if self.seats < 0:
-            # Can optionally return a warning and domains
-            return {
-                'warning': {
-                    'title': "No negative seats",
-                    'message': "The seats number must be a positive number",
-                }
-            }
+            return raise_warning("No negative seats",
+                     "The seats number must be a positive number")
         elif self.seats < len(self.attendee_ids):
-            return {
-                'warning': {
-                    'title': "Not enough seats",
-                    'message': "Too much attendees for the number of seats",
-                }
-            }
+            return raise_warning("Not enough seats",
+                    "Too much attendees for the number of seats")
+                    
+    @api.constrains('instructor_id', 'attendee_ids')
+    def _check_not_teaching_himself(self):
+        for record in self:
+            if record.instructor_id and record.instructor_id.id in record.attendee_ids.ids:
+                raise ValidationError("Instructor cannot teach himself")
+                
+    
+
     
